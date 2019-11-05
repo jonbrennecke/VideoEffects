f5e0821a0e9cca773a6fd4425e85080ca2b58cd4
Jon Brennecke <jpbrennecke@gmail.com>
2019-11-04 16:01:17 -0800

    added --allow-empty flag

--- scripts/set_version.sh
+++ scripts/set_version.sh
@@ -1,7 +1,7 @@
 
 #!/usr/bin/env zsh
 if [ -z "$(git status --porcelain)" ]; then 
-  git commit -am "v$1"
+  git commit -m --allow-empty "v$1"
   git tag "v$1"
 else 
   echo "Error: Git has uncommitted changes."
