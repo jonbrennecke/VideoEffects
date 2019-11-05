94a0e1a68fd7ab94c3b51867b3af087b465f53cb
Jon Brennecke <jpbrennecke@gmail.com>
2019-11-04 16:01:54 -0800

    fixed set_version script

--- scripts/set_version.sh
+++ scripts/set_version.sh
@@ -1,7 +1,7 @@
 
 #!/usr/bin/env zsh
 if [ -z "$(git status --porcelain)" ]; then 
-  git commit -m --allow-empty "v$1"
+  git commit --allow-empty -m "v$1"
   git tag "v$1"
 else 
   echo "Error: Git has uncommitted changes."
