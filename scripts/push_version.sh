22307a4b0557d4a62ebfa8ad76ae30d45cf28fcd
Jon Brennecke <jpbrennecke@gmail.com>
2019-11-04 18:45:05 -0800

    push version script

--- scripts/push_version.sh
+++ scripts/push_version.sh
@@ -4,6 +4,9 @@ version=$(cat version.txt)
 if [ -z "$(git status --porcelain)" ]; then 
   git commit --allow-empty -m "v$version"
   git tag "v$version"
+  git push origin master
+  git push origin master --tags
+  pod repo push private-pod-specs Captions.podspec --allow-warnings  
 else 
   echo "Error: Git has uncommitted changes."
   exit 1
